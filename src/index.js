e4a252d64f79c05202be471c0e6e9dcfa43aaf21
Rae40 <103543425+Rae40@users.noreply.github.com>
2024-01-27 17:40:22 -0500

    added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-function generateQuote(event) {
-  event.preventDefault();
+function displayQuote(response) {
 
+  console.log("quote generated")
   new Typewriter("#quote2", {
-    strings: ["Hello", "World"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "ff6b2813f5d6ff41a2413tcof35fc170";
+  let prompt= `User instructions: Generate a quote about ${instructionsInput.value} include the 'person who said it' in <strong> element`;
+  let context= "You are a language professor and to tell your students quotes during lessons. Your mission is to generate a quote in basic HTML. Please follow the user instructions.";
+  let apiURL =
+    `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+    console.log("Generating quote");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+  axios.get(apiURL).then(displayQuote);
+
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
